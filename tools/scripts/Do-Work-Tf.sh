f7752f33304b745ea2c0b8e35f2df5cb3534d21e
Greg Oliver <golive@microsoft.com>
2019-07-30 16:11:27 +0100

    terraform commands

--- tools/scripts/Do-Work-Tf.sh
+++ tools/scripts/Do-Work-Tf.sh
@@ -2,6 +2,7 @@ cd /home/scripts
 export ASSET_STANDARD_TYPE=$1
 export RG_NAME=$2
 export RG_LOCATION=$3
-terraform init -input=false
-terraform plan -out=tfplan -input=false
+
+terraform init -input=false ../templates/standard-$ASSET_STANDARD_TYPE/
+terraform plan -out=tfplan -input=false ../templates/standard-$ASSET_STANDARD_TYPE/
 terraform apply -input=false tfplan
