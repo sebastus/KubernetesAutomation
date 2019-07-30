8f5e3ca1a2c83bd554fa0e256ff7e81af0a5ea35
Greg Oliver <golive@microsoft.com>
2019-07-30 16:17:37 +0100

    add vars to tf plan

--- tools/scripts/Do-Work-Tf.sh
+++ tools/scripts/Do-Work-Tf.sh
@@ -4,5 +4,12 @@ export RG_NAME=$2
 export RG_LOCATION=$3
 
 terraform init -input=false ../templates/standard-$ASSET_STANDARD_TYPE/
-terraform plan -out=tfplan -input=false ../templates/standard-$ASSET_STANDARD_TYPE/
+terraform plan -out=tfplan -input=false  \
+    -var AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
+    -var AZURE_APP_ID=$AZURE_APP_ID \
+    -var AZURE_APP_KEY=$AZURE_APP_KEY \
+    -var AZURE_TENANT_ID=$AZURE_TENANT_ID
+    -var RG_NAME=$RG_NAME \
+    -var RG_LOCATION=$RG_LOCATION \
+    ../templates/standard-$ASSET_STANDARD_TYPE/
 terraform apply -input=false tfplan
