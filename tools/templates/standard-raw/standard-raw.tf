e9c4b8c7b45e901703b0765ec0adaf5d504fd6a7
Greg Oliver <golive@microsoft.com>
2019-07-30 15:57:53 +0100

    add env vars to tf

--- tools/templates/standard-raw/standard-raw.tf
+++ tools/templates/standard-raw/standard-raw.tf
@@ -1,9 +1,23 @@
-variable "resource_group_name"
-variable "location"
+variable "AZURE_APP_KEY" {}
+variable "AZURE_SUBSCRIPTION_ID" {}
+variable "AZURE_APP_ID" {}
+variable "AZURE_TENANT_ID"{}
+
+provider "azurerm" {
+  version = "~> 1.28.0"
+
+  subscription_id = "${var.AZURE_APP_KEY}"
+  client_id       = "${var.AZURE_SUBSCRIPTION_ID}"
+  client_secret   = "${var.AZURE_APP_ID}"
+  tenant_id       = "${var.AZURE_TENANT_ID}"
+}
+
+variable "RG_NAME" {}
+variable "RG_LOCATION" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "var.resource_group_name"
-  location = "var.location"
+  name     = "${var.RG_NAME}"
+  location = "${var.RG_LOCATION}"
 }
 
 resource "azurerm_databricks_workspace" "db" {
