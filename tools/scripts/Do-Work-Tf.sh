cbb560a268d539837ed89939f738532416452140
Greg Oliver <golive@microsoft.com>
2019-07-30 16:21:21 +0100

    fix command line

--- tools/scripts/Do-Work-Tf.sh
+++ tools/scripts/Do-Work-Tf.sh
@@ -8,7 +8,7 @@ terraform plan -out=tfplan -input=false  \
     -var AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
     -var AZURE_APP_ID=$AZURE_APP_ID \
     -var AZURE_APP_KEY=$AZURE_APP_KEY \
-    -var AZURE_TENANT_ID=$AZURE_TENANT_ID
+    -var AZURE_TENANT_ID=$AZURE_TENANT_ID \
     -var RG_NAME=$RG_NAME \
     -var RG_LOCATION=$RG_LOCATION \
     ../templates/standard-$ASSET_STANDARD_TYPE/
