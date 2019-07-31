b056dca8a1e6065ed6eec87d1759f42bf4a2e9af
Greg Oliver <golive@microsoft.com>
2019-07-31 14:14:08 +0100

    more configmap

--- tools/scripts/Do-Work-Tf.sh
+++ tools/scripts/Do-Work-Tf.sh
@@ -1,10 +1,6 @@
 cd /home/scripts
 
-export ASSET_STANDARD_TYPE=$1
-export RG_NAME=$2
-export RG_LOCATION=$3
-
-terraform init -input=false ../templates/standard-$ASSET_STANDARD_TYPE/
+terraform init -input=false ../templates/$ASSET_CONFIGURATION/
 
 terraform plan -out=tfplan -input=false  \
     -var AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
@@ -13,6 +9,6 @@ terraform plan -out=tfplan -input=false  \
     -var AZURE_TENANT_ID=$AZURE_TENANT_ID \
     -var RG_NAME=$RG_NAME \
     -var RG_LOCATION=$RG_LOCATION \
-    ../templates/standard-$ASSET_STANDARD_TYPE/
+    ../templates/$ASSET_CONFIGURATION/
 
 terraform apply -input=false tfplan
