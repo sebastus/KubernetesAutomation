a17b5e9f556ea0d51b4e867e28cad0c4037b3861
Greg Oliver <golive@microsoft.com>
2019-08-01 16:38:45 +0100

    add asset controller

---
+++ apiSolution/api/Classes/NewConfigmap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Classes
+{
+    public class NewConfigmap
+    {
+        public string rgName { get; set; }
+        public string rgLocation { get; set; }
+        public string assetConfiguration { get; set; }
+        public string azureTenantId { get; set; }
+        public string azureSubscriptionId { get; set; }
+    }
+}
---
+++ apiSolution/api/Controllers/AssetController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using api.Classes;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetController : ControllerBase
+    {
+        // POST api/asset
+        [HttpPost]
+        public void Post([FromBody] NewConfigmap value)
+        {
+        }
+
+
+    }
+}
